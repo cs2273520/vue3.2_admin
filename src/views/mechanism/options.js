f6998cba22245e188428581d3f4ac5fc4406608a
cs2273520 <1329413417@qq.com>
2023-01-04 14:32:51 +0800

    chore: 完善5

--- src/views/mechanism/options.js
+++ src/views/mechanism/options.js
@@ -44,3 +44,55 @@ export const typeoptions = [
     label: '个人独资企业'
   }
 ]
+
+export const exportInfoTitle = [
+  {
+    title: '机构名称',
+    key: 'name',
+    type: 'text'
+  },
+  {
+    title: '机构类型',
+    key: 'type',
+    type: 'text'
+  },
+  {
+    title: '提交时间',
+    key: 'submitDate',
+    type: 'text'
+  },
+  {
+    title: '审核时间',
+    key: 'auditDate',
+    type: 'text'
+  },
+  {
+    title: '持有者',
+    key: 'owner',
+    type: 'text'
+  },
+  {
+    title: '电话',
+    key: 'phone',
+    type: 'text'
+  },
+  {
+    title: '地址',
+    key: 'address',
+    type: 'text'
+  },
+  {
+    title: '证件1',
+    key: 'image1',
+    type: 'image',
+    width: 50,
+    height: 50
+  },
+  {
+    title: '证件2',
+    key: 'image2',
+    type: 'image',
+    width: 50,
+    height: 50
+  }
+]
